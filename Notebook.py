b6ed1425ee5918658a530936a2852f09662471f0
Kavya Katal <kavyakatal09@gmail.com>
2026-01-06 11:22:34 +0530

    Initialsing

---
+++ Notebook.py
@@ -0,0 +1,9 @@
+# Using jupiter notebook extension here
+import jupyterlab
+print("JupyterLab version:", jupyterlab.__version__)
+# Additional notebook-related code can be added here
+import notebook
+print("Notebook version:", notebook.__version__)
+# This code snippet imports the JupyterLab and Notebook modules and prints their versions.
+
+# You can add more functionality as needed for your notebook environment.
